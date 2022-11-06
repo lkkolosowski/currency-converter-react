12b4624737b5179db7cc4e8c4391cfb4007cb6c2
Lukasz Kolosowski <lkkolosowski@gmail.com>
2022-11-06 22:31:44 +0100

    added currencies data

---
+++ src/currencies.js
@@ -0,0 +1,74 @@
+export const currencies = [
+  {
+    code: "PLN",
+    name: "Złoty polski",
+    rate: 1,
+    flag: "pl",
+  },
+  {
+    code: "USD",
+    name: "Dolar amerykański",
+    rate: 0.2023,
+    flag: "um",
+  },
+  {
+    code: "EUR",
+    name: "Euro",
+    rate: 0.2079,
+    flag: "eu",
+  },
+  {
+    code: "JPY",
+    name: "Yen japoński",
+    rate: 29.1681,
+    flag: "jp",
+  },
+  {
+    code: "CZK",
+    name: "Korona czeska",
+    rate: 5.1282,
+    flag: "cz",
+  },
+  {
+    code: "HRK",
+    name: "Kuna chorwacka",
+    rate: 1.5663,
+    flag: "hr",
+  },
+  {
+    code: "CHF",
+    name: "Frank szwajcarski",
+    rate: 0.1975,
+    flag: "ch",
+  },
+  {
+    code: "GBP",
+    name: "Funt brytyjski",
+    rate: 0.1861,
+    flag: "gb",
+  },
+  {
+    code: "INR",
+    name: "Rupia indyjska",
+    rate: 16.4741,
+    flag: "in",
+  },
+  {
+    code: "SEK",
+    name: "Korona szwecka",
+    rate: 2.2645,
+    flag: "se",
+  },
+  {
+    code: "KRW",
+    name: "Won południowokoreański",
+    rate: 288.2037,
+    flag: "kr",
+  },
+  {
+    code: "CNY",
+    name: "Yuan chiński",
+    rate: 1.4568,
+    flag: "cn",
+  },
+];
