934be25d69e2daa2632c4314f5297d0a0a4810ed
Lukasz Kolosowski <lkkolosowski@gmail.com>
2022-11-07 01:31:37 +0100

    improved semantic and class names

--- src/Form/index.js
+++ src/Form/index.js
@@ -5,47 +5,55 @@ const Form = () => {
   return (
     <form className="form">
       <fieldset className="form__fieldset">
-      <legend align="center" className="form__legend">Konwerter walutowy</legend>
+        <legend align="center" className="form__legend">
+          Konwerter walutowy
+        </legend>
         <div className="form__wrapper">
           <div className="form__amount">
-            <p className="form__paragraph">Wprowadź ilość</p>
-            <input type="number" className="form__input js-amount" value="1" />
+            <label htmlFor="amount" className="form__label">
+              Wprowadź ilość
+            </label>
+            <input type="number" id="amount" className="form__input" value="1" />
           </div>
           <div className="form__col">
             <div className="form__row">
-              <p className="form__paragraph">Przelicz z</p>
+              <label htmlFor="from-currency" className="form__label">
+                Przelicz z
+              </label>
               <div className="form__select-box">
-                <select className="form__select">
+                <select id="from-currency" className="form__select">
                   {currencies.map(({ code }) => (
                     <option key={code} value={code}>
                       {code}
                     </option>
                   ))}
                 </select>
-                <p className="form__caption js-caption"></p>
+                <p className="form__caption"></p>
               </div>
             </div>
             <div className="form__row">
-              <p className="form__paragraph">
-                <i className="form__exchange-icon fas fa-exchange-alt js-exchange-icon"></i>
+              <p className="form__label">
+                <i className="form__exchange-icon fas fa-exchange-alt"></i>
               </p>
             </div>
             <div className="form__row">
-              <p className="form__paragraph">Przelicz na</p>
+              <label htmlFor="to-currency" className="form__label">
+                Przelicz na
+              </label>
               <div className="form__select-box">
-                <select className="form__select">
+                <select id="to-currency" className="form__select">
                   {currencies.map(({ code }) => (
                     <option key={code} value={code}>
                       {code}
                     </option>
                   ))}
                 </select>
-                <p className="form__caption js-caption"></p>
+                <p className="form__caption"></p>
               </div>
             </div>
           </div>
-          <p className="form__paragraph">Kurs wymiany</p>
-          <div className="form__result js-result-text"></div>
+          <p className="form__label">Kurs wymiany</p>
+          <div className="form__result"></div>
         </div>
         <p className="form__paragraph form__paragraph--small">Kurs walut został zaktualizowany w dniu 28.09.2022</p>
       </fieldset>
