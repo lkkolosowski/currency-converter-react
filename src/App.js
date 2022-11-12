49d4d958932fa051e37634b82d69518af7e1531d
Lukasz Kolosowski <lkkolosowski@gmail.com>
2022-11-12 01:05:19 +0100

    moved properties to the App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,74 @@
+import { useState } from "react";
 import Form from "./Form";
+import SelectBox from "./Form/SelectBox";
+import Input from "./Form/Input";
+import Button from "./Form/Button";
+import { currencies } from "./currencies.js";
 
 function App() {
-  return <Form />;
+  const [sourceCurrency, setSourceCurrency] = useState(currencies[0]);
+  const [targetCurrency, setTargetCurrency] = useState(currencies[1]);
+  const [amount, setAmount] = useState("1");
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const onKeyPress = (event) => {
+    if (event.code === "Minus") {
+      event.preventDefault();
+    }
+  };
+
+  const swapCurrencies = () => {
+    setSourceCurrency(targetCurrency);
+    setTargetCurrency(sourceCurrency);
+  };
+
+  const result = ((amount * targetCurrency.rate) / sourceCurrency.rate).toFixed(2);
+  return (
+    <Form
+      onSubmit={onSubmit}
+      amountInput={
+        <Input
+          type="number"
+          id="amount"
+          min="0"
+          disabled={false}
+          value={amount}
+          onChange={({ target }) => setAmount(target.value)}
+          onKeyPress={onKeyPress}
+        />
+      }
+      sourceCurrencySelectBox={
+        <SelectBox
+          src={`https://flagicons.lipis.dev/flags/4x3/${sourceCurrency.flag}.svg`}
+          id="source-currency"
+          value={sourceCurrency.code}
+          onChange={({ target }) => setSourceCurrency(currencies.find(({ code }) => code === target.value))}
+          currencyName={sourceCurrency.name}
+        />
+      }
+      button={<Button onClick={swapCurrencies} />}
+      targetCurrencySelectBox={
+        <SelectBox
+          src={`https://flagicons.lipis.dev/flags/4x3/${targetCurrency.flag}.svg`}
+          id="target-currency"
+          value={targetCurrency.code}
+          onChange={({ target }) => setTargetCurrency(currencies.find(({ code }) => code === target.value))}
+          currencyName={targetCurrency.name}
+        />
+      }
+      resultInput={
+        <Input
+          type="text"
+          id="result"
+          disabled={true}
+          value={`${amount} ${sourceCurrency.code} = ${result} ${targetCurrency.code}`}
+        />
+      }
+    />
+  );
 }
 
 export default App;
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,31 +1,7 @@
-import SelectBox from "./SelectBox";
-import Input from "./Input";
-import Button from "./Button";
-import { useState } from "react";
+
 import "./style.css";
-import { currencies } from "../currencies.js";
 
-const Form = () => {
-  const [sourceCurrency, setSourceCurrency] = useState(currencies[0]);
-  const [targetCurrency, setTargetCurrency] = useState(currencies[1]);
-  const [amount, setAmount] = useState("1");
-
-  const onSubmit = (event) => {
-    event.preventDefault();
-  };
-
-  const onKeyPress = (event) => {
-    if (event.code === "Minus") {
-      event.preventDefault();
-    }
-  };
-
-  const swapCurrencies = () => {
-    setSourceCurrency(targetCurrency);
-    setTargetCurrency(sourceCurrency);
-  };
-
-  const result = ((amount * targetCurrency.rate) / sourceCurrency.rate).toFixed(2);
+const Form = ({ onSubmit, amountInput, sourceCurrencySelectBox, button, targetCurrencySelectBox, resultInput }) => {
 
   return (
     <form className="form" onSubmit={onSubmit}>
@@ -37,53 +13,28 @@ const Form = () => {
           <label htmlFor="amount" className="form__label">
             Wprowadź ilość
           </label>
-          <Input
-            type="number"
-            id="amount"
-            min="0"
-            disabled={false}
-            value={amount}
-            onChange={({ target }) => setAmount(target.value)}
-            onKeyPress={onKeyPress}
-          />
+          {amountInput}
           <div className="form__column">
             <div className="form__row">
               <label htmlFor="source-currency" className="form__label">
                 Przelicz z
               </label>
-              <SelectBox
-                src={`https://flagicons.lipis.dev/flags/4x3/${sourceCurrency.flag}.svg`}
-                id="source-currency"
-                value={sourceCurrency.code}
-                onChange={({ target }) => setSourceCurrency(currencies.find(({ code }) => code === target.value))}
-                currencyName={sourceCurrency.name}
-              />
+              {sourceCurrencySelectBox}
             </div>
             <div className="form__row">
-              <Button onClick={swapCurrencies} />
+              {button}
             </div>
             <div className="form__row">
               <label htmlFor="target-currency" className="form__label">
                 Przelicz na
               </label>
-              <SelectBox
-                src={`https://flagicons.lipis.dev/flags/4x3/${targetCurrency.flag}.svg`}
-                id="target-currency"
-                value={targetCurrency.code}
-                onChange={({ target }) => setTargetCurrency(currencies.find(({ code }) => code === target.value))}
-                currencyName={targetCurrency.name}
-              />
+             {targetCurrencySelectBox}
             </div>
           </div>
           <label htmlFor="result" className="form__label">
             Kurs wymiany
           </label>
-          <Input
-            type="text"
-            id="result"
-            disabled={true}
-            value={`${amount} ${sourceCurrency.code} = ${result} ${targetCurrency.code}`}
-          />
+          {resultInput}
         </div>
         <p className="form__paragraph form__paragraph--small">Kurs walut został zaktualizowany w dniu 28.09.2022</p>
       </fieldset>
