5739f71bbf3055b0ad44f1d2f48ccd2a173e0254
Lukasz Kolosowski <lkkolosowski@gmail.com>
2022-11-12 01:31:32 +0100

    formatted

--- src/Form/Input/index.js
+++ src/Form/Input/index.js
@@ -2,16 +2,16 @@ import "./style.css";
 
 const Input = ({ type, id, min, disabled, value, onChange, onKeyPress }) => {
   return (
-      <input
-        type={type}
-        id={id}
-        min={min}
-        disabled={disabled}
-        className="input"
-        value={value}
-        onChange={onChange}
-        onKeyPress={onKeyPress}
-      />
+    <input
+      type={type}
+      id={id}
+      min={min}
+      disabled={disabled}
+      className="input"
+      value={value}
+      onChange={onChange}
+      onKeyPress={onKeyPress}
+    />
   );
 };
 
