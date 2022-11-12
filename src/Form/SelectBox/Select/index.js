85999e73523019875dfcd89893dd61b6aa8338e4
Lukasz Kolosowski <lkkolosowski@gmail.com>
2022-11-12 01:13:47 +0100

    moved select styles to separate style.css

--- src/Form/SelectBox/Select/index.js
+++ src/Form/SelectBox/Select/index.js
@@ -1,8 +1,9 @@
 import { currencies } from "../../../currencies.js";
+import "./style.css";
 
 const Select = ({ id, value, onChange }) => {
   return (
-    <select id={id} className="form__select" value={value} onChange={onChange}>
+    <select id={id} className="select" value={value} onChange={onChange}>
       {currencies.map(({ code }) => (
         <option key={code} value={code}>
           {code}
