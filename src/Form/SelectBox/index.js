d5433c421323f87227ae518e21d0abfc1f5229b2
Lukasz Kolosowski <lkkolosowski@gmail.com>
2022-11-12 01:10:30 +0100

    moved selectBox styles to separate style.css

--- src/Form/SelectBox/index.js
+++ src/Form/SelectBox/index.js
@@ -1,10 +1,11 @@
 import Select from "./Select";
 import Flag from "./Flag";
 import Caption from "./Caption";
+import "./style.css";
 
 const SelectBox = ({ src, id, value, onChange, currencyName }) => {
   return (
-    <div className="form__select-box">
+    <div className="selectBox">
       <Flag src={src} />
       <Select id={id} value={value} onChange={onChange} />
       <Caption currencyName={currencyName} />
