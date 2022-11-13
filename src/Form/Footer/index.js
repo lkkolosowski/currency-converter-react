49d10933bb28c626b1daf6e80199c9a84bfd3eb5
Lukasz Kolosowski <lkkolosowski@gmail.com>
2022-11-13 05:53:23 +0100

    formatted content of <Footer> component

--- src/Form/Footer/index.js
+++ src/Form/Footer/index.js
@@ -3,8 +3,11 @@ import "./style.css";
 const Footer = ({ clock }) => {
   return (
     <div className="footer">
-      <p className="footer__paragraph">Kurs zaktualizowano w dniu 28.09.2022</p>
-      <p className="footer__paragraph">{clock}</p>
+      <p className="footer__paragraph">
+        <span>Kurs&nbsp;walut&nbsp;zaktualizowano </span>
+        <span>w&nbsp;dniu 28&nbsp;października&nbsp;2022</span>
+      </p>
+      {clock}
     </div>
   );
 };
