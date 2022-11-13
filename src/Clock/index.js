d9f7e798b87d8d2ada08a009651e72e122201b31
Lukasz Kolosowski <lkkolosowski@gmail.com>
2022-11-13 08:06:51 +0100

    renamed constant and function

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -1,11 +1,11 @@
 import { useState, useEffect } from "react";
 
 const Clock = () => {
-  const [date, setDate] = useState(new Date());
+  const [newDate, setNewDate] = useState(new Date());
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      setDate(new Date());
+      setNewDate(new Date());
     }, 1000);
 
     return () => {
@@ -17,20 +17,20 @@ const Clock = () => {
     <span className="clock">
       <span>
         Dzisiaj&nbsp;jest&nbsp;
-        {date.toLocaleDateString(undefined, { weekday: "long" })}
+        {newDate.toLocaleDateString(undefined, { weekday: "long" })}
         {", "}
       </span>
       <span>
-        {date
+        {newDate
           .toLocaleDateString(undefined, { day: "numeric", month: "long" })
           .split(" ")
           .map((element) => (
             <span key={element}>{element}&nbsp;</span>
           ))}
-        {date.toLocaleDateString(undefined, { year: "numeric" })}
+        {newDate.toLocaleDateString(undefined, { year: "numeric" })}
         {", "}
       </span>
-      <span>{date.toLocaleTimeString()}</span>
+      <span>{newDate.toLocaleTimeString()}</span>
     </span>
   );
 };
