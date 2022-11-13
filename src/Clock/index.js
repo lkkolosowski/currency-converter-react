056d241c8b211ebf2b82db37e78bedbe421acf32
Lukasz Kolosowski <lkkolosowski@gmail.com>
2022-11-13 05:54:14 +0100

    formatted content of <Clock> component

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -1,7 +1,28 @@
-const Clock = () => {
-  const myDate = new Date();
+import "./style.css";
 
-  return <span className="date">{myDate.toString()}</span>;
+const Clock = () => {
+  const newDate = new Date();
+
+  return (
+    <p className="clock">
+      <span>
+        Dzisiaj&nbsp;jest&nbsp;
+        {newDate.toLocaleDateString(undefined, { weekday: "long" })}
+        {", "}
+      </span>
+      <span>
+        {newDate
+          .toLocaleDateString(undefined, { day: "numeric", month: "long" })
+          .split(" ")
+          .map((element) => (
+            <span>{element}&nbsp;</span>
+          ))}
+        {newDate.toLocaleDateString(undefined, { year: "numeric" })}
+        {", "}
+      </span>
+      <span>{newDate.toLocaleTimeString()}</span>
+    </p>
+  );
 };
 
 export default Clock;
