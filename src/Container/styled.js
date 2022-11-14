33419bd204683b1d3541424bbdd1c4a4031a030b
Lukasz Kolosowski <lkkolosowski@gmail.com>
2022-11-14 04:37:15 +0100

    added styled.js to <container> component

---
+++ src/Container/styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import background from "../background.jpg";
+
+export const Main = styled.main`
+  display: flex;
+  align-items: center;
+  min-height: 100vh;
+  background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url("${background}");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  justify-content: center;
+`;
