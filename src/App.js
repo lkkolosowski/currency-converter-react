d73c70f8bf9dc7c3bd483eb03e3cdec9025ec5d5
Lukasz Kolosowski <lkkolosowski@gmail.com>
2022-11-22 04:42:21 +0100

    hidden status bar

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import SwapButton from "./Form/SwapButton";
 import SubmitButton from "./Form/SubmitButton";
 import Footer from "./Form/Footer";
 import { currencies } from "./currencies.js";
+import { SafeAreaView, StatusBar } from "react-native";
 
 function App() {
   const [sourceCurrency, setSourceCurrency] = useState(currencies[0]);
@@ -45,58 +46,61 @@ function App() {
   const dateFormat = undefined;
 
   return (
-    <Container>
-      <Form
-        clock={<Clock dateFormat={dateFormat} />}
-        onSubmit={onSubmit}
-        amountInput={
-          <Input
-            autoFocus={true}
-            type="number"
-            id="amount"
-            min="0"
-            step="0.01"
-            disabled={false}
-            value={amount}
-            onChange={({ target }) => setAmount(target.value)}
-            onKeyPress={onKeyPress}
-          />
-        }
-        sourceCurrencySelectBox={
-          <SelectBox
-            src={`https://flagicons.lipis.dev/flags/4x3/${sourceCurrency.flag}.svg`}
-            id="source-currency"
-            value={sourceCurrency.code}
-            onChange={({ target }) => setSourceCurrency(currencies.find(({ code }) => code === target.value))}
-            currencyName={sourceCurrency.name}
-          />
-        }
-        targetCurrencySelectBox={
-          <SelectBox
-            src={`https://flagicons.lipis.dev/flags/4x3/${targetCurrency.flag}.svg`}
-            id="target-currency"
-            value={targetCurrency.code}
-            onChange={({ target }) => setTargetCurrency(currencies.find(({ code }) => code === target.value))}
-            currencyName={targetCurrency.name}
-          />
-        }
-        swapButton={<SwapButton onClick={swapCurrencies} content={<i className="fas fa-exchange-alt"></i>} />}
-        resultInput={
-          <Input
-            type="text"
-            id="result"
-            disabled={true}
-            value={`${
-              result.sourceAmount !== undefined
-                ? `${result.sourceAmount} ${result.sourceCode} = ${result.targetAmount} ${result.targetCode}`
-                : ``
-            }`}
-          />
-        }
-        submitButton={<SubmitButton content="Przelicz" />}
-        footer={<Footer dateFormat={dateFormat} />}
-      />
-    </Container>
+    <SafeAreaView>
+      <StatusBar hidden/>
+      <Container>
+        <Form
+          clock={<Clock dateFormat={dateFormat} />}
+          onSubmit={onSubmit}
+          amountInput={
+            <Input
+              autoFocus={true}
+              type="number"
+              id="amount"
+              min="0"
+              step="0.01"
+              disabled={false}
+              value={amount}
+              onChange={({ target }) => setAmount(target.value)}
+              onKeyPress={onKeyPress}
+            />
+          }
+          sourceCurrencySelectBox={
+            <SelectBox
+              src={`https://flagicons.lipis.dev/flags/4x3/${sourceCurrency.flag}.svg`}
+              id="source-currency"
+              value={sourceCurrency.code}
+              onChange={({ target }) => setSourceCurrency(currencies.find(({ code }) => code === target.value))}
+              currencyName={sourceCurrency.name}
+            />
+          }
+          targetCurrencySelectBox={
+            <SelectBox
+              src={`https://flagicons.lipis.dev/flags/4x3/${targetCurrency.flag}.svg`}
+              id="target-currency"
+              value={targetCurrency.code}
+              onChange={({ target }) => setTargetCurrency(currencies.find(({ code }) => code === target.value))}
+              currencyName={targetCurrency.name}
+            />
+          }
+          swapButton={<SwapButton onClick={swapCurrencies} content={<i className="fas fa-exchange-alt"></i>} />}
+          resultInput={
+            <Input
+              type="text"
+              id="result"
+              disabled={true}
+              value={`${
+                result.sourceAmount !== undefined
+                  ? `${result.sourceAmount} ${result.sourceCode} = ${result.targetAmount} ${result.targetCode}`
+                  : ``
+              }`}
+            />
+          }
+          submitButton={<SubmitButton content="Przelicz" />}
+          footer={<Footer dateFormat={dateFormat} />}
+        />
+      </Container>
+    </SafeAreaView>
   );
 }
 
