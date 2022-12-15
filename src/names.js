0c8e94fc8d720d9754708b572a890477f6c6d485
Lukasz Kolosowski <lkkolosowski@gmail.com>
2022-12-15 21:12:21 +0100

    create file names.js

---
+++ src/names.js
@@ -0,0 +1,62 @@
+export const names = [
+  {
+    code: "PLN",
+    name: "Złoty polski",
+    flag: "pl",
+  },
+  {
+    code: "USD",
+    name: "Dolar amerykański",
+    flag: "um",
+  },
+  {
+    code: "EUR",
+    name: "Euro",
+    flag: "eu",
+  },
+  {
+    code: "JPY",
+    name: "Yen japoński",
+    flag: "jp",
+  },
+  {
+    code: "CZK",
+    name: "Korona czeska",
+    flag: "cz",
+  },
+  {
+    code: "HRK",
+    name: "Kuna chorwacka",
+    flag: "hr",
+  },
+  {
+    code: "CHF",
+    name: "Frank szwajcarski",
+    flag: "ch",
+  },
+  {
+    code: "GBP",
+    name: "Funt brytyjski",
+    flag: "gb",
+  },
+  {
+    code: "INR",
+    name: "Rupia indyjska",
+    flag: "in",
+  },
+  {
+    code: "SEK",
+    name: "Korona szwecka",
+    flag: "se",
+  },
+  {
+    code: "KRW",
+    name: "Won południowokoreański",
+    flag: "kr",
+  },
+  {
+    code: "CNY",
+    name: "Yuan chiński",
+    flag: "cn",
+  },
+];
