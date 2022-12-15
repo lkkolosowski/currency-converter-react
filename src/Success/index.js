2d1a115926e108ce3465c6bbc472861feedf959a
Lukasz Kolosowski <lkkolosowski@gmail.com>
2022-12-15 21:15:14 +0100

    create <Success> component

---
+++ src/Success/index.js
@@ -0,0 +1,18 @@
+import { Paragraph, Span } from "./styled";
+
+const Success = ({ currencies }) => {
+
+  return (
+    <Paragraph>
+      <Span>
+        Wszystko poprawnie 👌
+        <br />
+        (success)
+        <br />
+        {`${currencies[9].code}`}{` `}{`${currencies[9].name}`}
+      </Span>
+    </Paragraph>
+  );
+};
+
+export default Success;
