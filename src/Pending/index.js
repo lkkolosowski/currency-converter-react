35ccc13afbaae67353b7d83bf7062e4a237ba480
Lukasz Kolosowski <lkkolosowski@gmail.com>
2022-12-15 21:15:04 +0100

    create <Pending> component

---
+++ src/Pending/index.js
@@ -0,0 +1,15 @@
+import { Paragraph, Span } from "./styled";
+
+const Pending = () => {
+  return (
+    <Paragraph>
+      <Span>
+        Czekaj ...
+        <br />
+        (pending)
+      </Span>
+    </Paragraph>
+  );
+};
+
+export default Pending;
