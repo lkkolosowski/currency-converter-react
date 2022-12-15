a9aa8d4638d412f8d6024136fcbb30c72a3c5501
Lukasz Kolosowski <lkkolosowski@gmail.com>
2022-12-15 21:22:42 +0100

    function transfer to primary level

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,44 +1,110 @@
+import { useState } from "react";
+import Clock from "../Clock";
+import Input from "./Input";
+import SelectBox from "./SelectBox";
+import SwapButton from "./SwapButton";
+import SubmitButton from "./SubmitButton";
+import Footer from "./Footer";
 import { FormField, Fieldset, Section, PrimarySection, Title, Wrapper, Label, Column, Row } from "./styled";
 
-const Form = ({
-  onSubmit,
-  amountInput,
-  sourceCurrencySelectBox,
-  swapButton,
-  targetCurrencySelectBox,
-  resultInput,
-  submitButton,
-  clock,
-  footer,
-}) => {
+const Form = ({ currencies }) => {
+  const [sourceCurrency, setSourceCurrency] = useState("USD");
+  const [targetCurrency, setTargetCurrency] = useState("PLN");
+  const [amount, setAmount] = useState("1");
+  const [result, setResult] = useState("");
+
+  const calculateResult = () => {
+    const rate = +targetCurrency.rate / +sourceCurrency.rate;
+
+    setResult({
+      sourceAmount: +amount,
+      sourceCode: sourceCurrency.code,
+      targetAmount: (+amount * rate).toFixed(2),
+      targetCode: targetCurrency.code,
+    });
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    calculateResult();
+  };
+
+  const onKeyPress = (event) => {
+    if (event.code === "Minus") {
+      event.preventDefault();
+    }
+  };
+
+  const swapCurrencies = () => {
+    setSourceCurrency(targetCurrency);
+    setTargetCurrency(sourceCurrency);
+  };
+
+  const dateFormat = undefined;
+
   return (
     <FormField onSubmit={onSubmit}>
       <Fieldset>
         <Section as="header" align="right">
-          {clock}
+          <Clock dateFormat={dateFormat} />
         </Section>
         <PrimarySection>
           <Title>Konwerter walutowy</Title>
           <Wrapper className="form__wrapper">
             <Label htmlFor="amount">Wprowadź ilość</Label>
-            {amountInput}
+            <Input
+              autoFocus={true}
+              type="number"
+              id="amount"
+              min="0"
+              step="1"
+              disabled={false}
+              value={amount}
+              onChange={({ target }) => setAmount(target.value)}
+              onKeyPress={onKeyPress}
+            />
             <Column>
               <Row>
                 <Label htmlFor="source-currency">Przelicz z</Label>
-                {sourceCurrencySelectBox}
+                <SelectBox
+                  src={`https://flagicons.lipis.dev/flags/4x3/${sourceCurrency.flag}.svg`}
+                  id="source-currency"
+                  value={sourceCurrency.code}
+                  onChange={({ target }) => setSourceCurrency(currencies.find(({ code }) => code === target.value))}
+                  currencyName={sourceCurrency.name}
+                />
               </Row>
-              <Row>{swapButton}</Row>
+              <Row>
+                <SwapButton onClick={swapCurrencies} content={<i className="fas fa-exchange-alt"></i>} />
+              </Row>
               <Row>
                 <Label htmlFor="target-currency">Przelicz na</Label>
-                {targetCurrencySelectBox}
+                <SelectBox
+                  src={`https://flagicons.lipis.dev/flags/4x3/${targetCurrency.flag}.svg`}
+                  id="target-currency"
+                  value={targetCurrency.code}
+                  onChange={({ target }) => setTargetCurrency(currencies.find(({ code }) => code === target.value))}
+                  currencyName={targetCurrency.name}
+                />
               </Row>
             </Column>
-            {submitButton}
+            <SubmitButton content="Przelicz" />
             <Label htmlFor="result">Kurs wymiany</Label>
-            {resultInput}
+            <Input
+              type="text"
+              id="result"
+              disabled={true}
+              value={`${
+                result.sourceAmount !== undefined
+                  ? `${result.sourceAmount} ${result.sourceCode} = ${result.targetAmount} ${result.targetCode}`
+                  : ``
+              }`}
+            />
           </Wrapper>
         </PrimarySection>
-        <Section as="footer">{footer}</Section>
+        <Section as="footer">
+          <Footer dateFormat={dateFormat} />
+        </Section>
       </Fieldset>
     </FormField>
   );
