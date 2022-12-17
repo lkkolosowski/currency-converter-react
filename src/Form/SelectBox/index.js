1f32841a8de98da3c2befca3f51180c3ca7cfb56
Lukasz Kolosowski <lkkolosowski@gmail.com>
2022-12-18 00:02:59 +0100

    code format

--- src/Form/SelectBox/index.js
+++ src/Form/SelectBox/index.js
@@ -3,11 +3,11 @@ import Flag from "./Flag";
 import Caption from "./Caption";
 import { Box } from "./styled";
 
-const SelectBox = ({ src, id, value, onChange, currencyName, currencies}) => {
+const SelectBox = ({ src, id, value, onChange, currencyName, currencies }) => {
   return (
     <Box>
       <Flag src={src} />
-      <Select id={id} value={value} onChange={onChange} currencies={currencies}/>
+      <Select id={id} value={value} onChange={onChange} currencies={currencies} />
       <Caption currencyName={currencyName} />
     </Box>
   );
