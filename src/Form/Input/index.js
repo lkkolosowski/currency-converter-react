bd4eae86f8d0b327d97bc22c1e539e5faa1b00f3
Lukasz Kolosowski <lkkolosowski@gmail.com>
2022-12-18 00:03:29 +0100

    remove autofocus from input

--- src/Form/Input/index.js
+++ src/Form/Input/index.js
@@ -1,9 +1,8 @@
 import { InputField } from "./styled";
 
-const Input = ({ autoFocus, type, id, min,step, disabled, value, onChange, onKeyPress }) => {
+const Input = ({ type, id, min,step, disabled, value, onChange, onKeyPress }) => {
   return (
     <InputField
-      autoFocus={autoFocus}
       type={type}
       id={id}
       min={min}
