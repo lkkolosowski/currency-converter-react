86597aa1b04c9246ee56a53773f77b59745271d1
Lukasz Kolosowski <lkkolosowski@gmail.com>
2022-12-18 14:57:42 +0100

    simplify logic of props

--- src/Form/Footer/index.js
+++ src/Form/Footer/index.js
@@ -1,12 +1,12 @@
 import { Paragraph, Day} from "./styled";
 
-const Footer = ({ currenciesDay, dateFormat }) => {
+const Footer = ({ currenciesData, dateFormat }) => {
   return (
     <Paragraph>
       <span>Kurs walut zaktualizowano </span>
       <span>w dniu </span>
       <Day>
-        {new Date(currenciesDay.replaceAll("-", ", ")).toLocaleDateString(dateFormat, { day: "numeric", month: "long", year: "numeric" })}
+        {new Date(currenciesData.date.replaceAll("-", ", ")).toLocaleDateString(dateFormat, { day: "numeric", month: "long", year: "numeric" })}
       </Day>
     </Paragraph>
   );
