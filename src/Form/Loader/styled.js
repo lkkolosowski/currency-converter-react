ee705102f7a074dcb1ad337fa4743ac910cae74a
Lukasz Kolosowski <lkkolosowski@gmail.com>
2022-12-18 18:30:34 +0100

    change styles of Loader component

--- src/Form/Loader/styled.js
+++ src/Form/Loader/styled.js
@@ -1,11 +1,11 @@
 import styled from "styled-components";
 
 export const StyledLoader = styled.div`
-  border: 2px solid #f3f3f3;
+  border: 3px solid #f3f3f3;
   border-radius: 50%;
-  border-top: 2px solid #333;
-  width: 16px;
-  height: 16px;
+  border-top: 3px solid #333;
+  width: 24px;
+  height: 24px;
   -webkit-animation: spin 0.5s linear infinite; /* Safari */
   animation: spin 0.5s linear infinite;
 
