c887570a11b5097539461d025cbc083ec3e8be92
Lukasz Kolosowski <lkkolosowski@gmail.com>
2022-12-18 18:30:16 +0100

    rebuild Error component

--- src/Error/styled.js
+++ src/Error/styled.js
@@ -1,16 +1,6 @@
 import styled from "styled-components";
 
-export const Paragraph = styled.p`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin: 0;
-  width: 400px;
-  padding: 100px 20px;
-  border-radius: 30px;
-  color: #fff;
-  background-color: #dd4124;
+export const StyledError = styled.p`
+  color: #c80000;
   text-align: center;
 `;
-
-export const Span = styled.span([]);
