4f643979f95559c6d11aac68776c2229013c086a
Lukasz Kolosowski <lkkolosowski@gmail.com>
2022-12-21 23:32:10 +0100

    cosmetic change for nicer look of Error

--- src/Form/Error/styled.js
+++ src/Form/Error/styled.js
@@ -3,4 +3,9 @@ import styled from "styled-components";
 export const StyledError = styled.div`
   color: #c80000;
   text-align: center;
+  padding: 0 85px;
+
+  @media (max-width: 575px) {
+    padding: 0;
+  }
 `;
