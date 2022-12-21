2699c4498f352d6925349b000991180d479462ea
Lukasz Kolosowski <lkkolosowski@gmail.com>
2022-12-22 00:50:57 +0100

    delete rates.json

--- src/useCurrencies.js
+++ src/useCurrencies.js
@@ -10,7 +10,6 @@ export const useCurrencies = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        // const response = await axios.get("rates.json");
         const response = await axios.get(
           `https://api.exchangerate.host/latest?symbols=PLN,USD,EUR,JPY,CZK,HRK,CHF,GBP,INR,SEK,KRW,CNY&base=PLN&${Math.floor(
             Math.random() * 9999
