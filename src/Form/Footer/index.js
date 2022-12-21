6a2ef4d16d5868bfc43f6612d5fb89549f41db53
Lukasz Kolosowski <lkkolosowski@gmail.com>
2022-12-21 23:32:49 +0100

    cosmetic change in Footer

--- src/Form/Footer/index.js
+++ src/Form/Footer/index.js
@@ -1,18 +1,20 @@
 import { Paragraph, Day } from "./styled";
 
-const Footer = ({ currenciesData, dateFormat }) => {
+const Footer = ({ status, date, dateFormat }) => {
   return (
     <Paragraph>
       <span>Kurs walut zaktualizowano </span>
       <span>w dniu </span>
       <Day>
-        {currenciesData.status === "success"
-          ? new Date(currenciesData.date.replaceAll("-", ", ")).toLocaleDateString(dateFormat, {
+        {status === "success"
+          ? new Date(date.replaceAll("-", ", ")).toLocaleDateString(dateFormat, {
               day: "numeric",
               month: "long",
               year: "numeric",
             })
-          : "..."}
+          : status === "error"
+          ? `<błąd>`
+          : `...`}
       </Day>
     </Paragraph>
   );
