9329a425919aa38272a0ccf9f81eb75f0783a7c7
Lukasz Kolosowski <lkkolosowski@gmail.com>
2022-12-22 04:00:10 +0100

    add line-height to text in Error component

--- src/Form/Error/styled.js
+++ src/Form/Error/styled.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const StyledError = styled.div`
   color: #c80000;
   text-align: center;
+  line-height: 1.5;
   padding: 0 85px;
 
   @media (max-width: 575px) {
