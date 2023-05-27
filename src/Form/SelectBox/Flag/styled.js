0c0d40516dcfa1c425700278af35ab2ca02e5fe4
lkkolosowski <lkkolosowski@gmail.com>
2023-05-27 21:34:02 +0200

    cosmetic change

--- src/Form/SelectBox/Flag/styled.js
+++ src/Form/SelectBox/Flag/styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Image = styled.img`
   grid-area: flag;
-  width: 24px;
+  width: 23px;
   margin: 1px;
   aspect-ratio: 4 / 3;
   outline: 1px solid #888;
