aaeb733959bb87ab8136523be7c9971d007859a2
lkkolosowski <lkkolosowski@gmail.com>
2023-05-27 21:12:38 +0200

    cosmetic change

--- src/Form/SelectBox/Flag/styled.js
+++ src/Form/SelectBox/Flag/styled.js
@@ -2,8 +2,10 @@ import styled from "styled-components";
 
 export const Image = styled.img`
   grid-area: flag;
-  max-width: 25px;
-  border: 1px solid #888;
+  width: 24px;
+  margin: 1px;
+  aspect-ratio: 4 / 3;
+  outline: 1px solid #888;
   border-radius: 5px;
   display: block;
 `;
