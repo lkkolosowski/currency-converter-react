fdc1be12bcec0e7424d0de9599c82d87e0ba522a
lkkolosowski <lkkolosowski@gmail.com>
2023-06-14 23:49:40 +0200

    format code

--- src/Form/Footer/index.js
+++ src/Form/Footer/index.js
@@ -3,17 +3,24 @@ import { Paragraph, Day } from "./styled";
 const Footer = ({ status, date, dateFormat }) => {
   return (
     <Paragraph>
-      {status === "success" ? <><span>Kurs walut zaktualizowano </span>
-      <span>w dniu </span>
-      <Day>
-        {new Date(date.replaceAll("-", ", ")).toLocaleDateString(dateFormat, {
-              day: "numeric",
-              month: "long",
-              year: "numeric",
-            })
-         }
-      </Day></> : ""}
-      
+      {status === "success" ? (
+        <>
+          <span>Kurs walut zaktualizowano </span>
+          <span>w dniu </span>
+          <Day>
+            {new Date(date.replaceAll("-", ", ")).toLocaleDateString(
+              dateFormat,
+              {
+                day: "numeric",
+                month: "long",
+                year: "numeric",
+              }
+            )}
+          </Day>
+        </>
+      ) : (
+        ""
+      )}
     </Paragraph>
   );
 };
